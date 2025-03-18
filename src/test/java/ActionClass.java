c45a95b85a956aaa47385ba467db9d6c14ffc9d6
akash0233 <akashkale0611@gmail.com>
2025-03-18 15:05:50 +0530

    Added testng.xml file with @Test annotation use

--- src/test/java/ActionClass.java
+++ src/test/java/ActionClass.java
@@ -3,12 +3,18 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class ActionClass {
-    public static void main(String[] args) {
+    @Test
+    public void sampleTest(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
 
@@ -16,5 +22,6 @@ public class ActionClass {
         WebElement serach = driver.findElement(By.xpath("//textarea[@title='Search']"));
         a.moveToElement(serach).click().keyDown(Keys.SHIFT).sendKeys("india").doubleClick().build().perform();
         // varti shift key press karun india type kely jene karun Capital mdhe type hoeil INDIA
+
     }
 }
